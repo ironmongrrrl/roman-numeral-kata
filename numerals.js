78e086fecbb5c2390cac3544a41a2fabb5847f0f
Lucy Ironmonger <37961811+ironmongrrrl@users.noreply.github.com>
2021-07-02 23:58:57 +0100

    Create numerals.js

---
+++ numerals.js
@@ -0,0 +1,29 @@
+const numeralsLookUp = {
+  1000: "M",
+  500: "D",
+  100: "C",
+  50: "L",
+  10: "X",
+  9: "IX",
+  5: "V",
+  4: "IV",
+  1: "I",
+};
+
+let numeral = "";
+
+function numerals(number) {
+  Object.entries(numeralsLookUp)
+    .reverse()
+    .forEach(([key, value]) => {
+      if (number / key >= 1 || number > 0) {
+        while (number >= key) {
+          number = number - key;
+          numeral += value;
+        }
+      }
+    });
+  return numeral;
+}
+
+module.exports = numerals;
